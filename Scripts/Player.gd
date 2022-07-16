b0620d173a26972a03a528dfcd5681dcf41ecd5d
tylercchase <tylercchase@gmail.com>
2022-07-15 22:35:11 -0400

    Progress

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 export (int) var speed = 400
 export (int) var health = 3
 var velocity = Vector2()
-
+onready var projectile = preload("res:///Scenes/Projectile")
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
@@ -26,3 +26,6 @@ func on_hit(damage: int):
 	Events.emit_signal("set_health", health)
 	if health <= 0:
 		Events.emit_signal("player_death")
+
+func shoot():
+	pass
