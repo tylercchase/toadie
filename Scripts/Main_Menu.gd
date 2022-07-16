6aef78c1702584efdc46215cef0d4d4e0d183159
tylercchase <tylercchase@gmail.com>
2022-07-16 13:49:32 -0400

    Bullets around toad

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -11,10 +11,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://Scenes/World.tscn")
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,8 +13,8 @@ onready var projectile = preload("res:///Scenes/Projectiles/Projectile.tscn")
 func _ready():
 	Events.connect("enemy_death", self, "process_kill")
 
-func process_kill(health):
-	current_xp += health * 4 * xp_multiplier
+func process_kill(h):
+	current_xp += h * 4 * xp_multiplier
 	while current_xp >= next_xp:
 		current_xp -= next_xp
 		next_xp *= 2
@@ -32,27 +32,25 @@ func get_input():
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
-	look_at(get_global_mouse_position())
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 
-
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		var BULLET = projectile.instance()
-		BULLET.global_position = $Position2D.global_position
-		BULLET.rotation_degrees = rotation_degrees
-		BULLET.apply_impulse(Vector2(), Vector2(400, 0).rotated(rotation))
+		var pos = get_global_mouse_position() - global_position
+		var angle = atan2(pos.y, pos.x)
+		var position = Vector2(200 * cos(angle), 200 * sin(angle))
+		BULLET.global_position = global_position + position
+		BULLET.rotation_degrees = angle
+		BULLET.apply_impulse(Vector2(), Vector2(400, 0).rotated(angle))
 		get_node("/root/World/Projectiles").add_child(BULLET)
-		
+	
 func on_hit(damage: int):
 	print(damage)
 	health -= damage
 	Events.emit_signal("set_health", health)
 	if health <= 0:
 		Events.emit_signal("player_death")
-
-func shoot():
-	pass
